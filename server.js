e05301ceddfba58f6d351e2d0159b83bd448eb45
kitmikai <matara.timothy@gmail.com>
2020-07-22 16:33:42 +0300

    Connect to mongo database

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 
 const app = express();
 
 const port = 3000;
+
+// Connect to database
+let mongoDbUrl = "mongodb://localhost/";
+let dbName = "moorings";
+const connString = mongoDbUrl + dbName;
+
+mongoose.connect(
+  connString,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("Database connected successfully");
+    }
+  }
+);
 
 // Middlewares
 app.use(express.static(path.join(__dirname, "./static")));
