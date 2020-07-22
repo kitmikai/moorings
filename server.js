0d59cede82a210d1239a32c3b22476acbef616b8
kitmikai <matara.timothy@gmail.com>
2020-07-22 13:15:25 +0300

    Initial image files and project setup for node.js

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 const port = 3000;
 
+// Middlewares
+app.use(express.static(path.join(__dirname, "./static")));
+
 // Route for the index page
 app.get("/", (request, response) => {
-  response.send("Hello Express:-D");
+  response.sendFile(path.join(__dirname, "./static/index.html"));
 });
 
 app.listen(port, () => {
+  console.log("Restarting");
   console.log(`Express server listening on port ${port}!`);
 });
